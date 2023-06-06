cefabf403df2337c5accd1cb8eb5b0eae34ed416
Advait <Advait.Mishra@geminiSolutions.com>
2023-06-06 13:15:05 +0530

    create update delete

---
+++ ApprovalWorkflows/ApprovalWorkflows.BO/DTOs/AddApprovalWorkflowDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprovalWorkflows.BO.DTOs
+{
+    public class AddApprovalWorkflowDto
+    {
+        public int TemplateID { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Template { get; set; }
+    }
+}
--- ApprovalWorkflows/ApprovalWorkflows.DAL/Contract/IApprovalWorkflowRepository.cs
+++ ApprovalWorkflows/ApprovalWorkflows.DAL/Contract/IApprovalWorkflowRepository.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApprovalWorkflows.BO.DTOs;
 using ApprovalWorkflows.BO.Entites;
+
 
 namespace ApprovalWorkflows.DAL.Contract
 {
+   
     public interface IApprovalWorkflowRepository
     {
         public List<ApiBusinessObjects> GetAllApprovalWorkflow();
+        public UpdateApprovalWorkflowDto UpdateApprovalWorkflow(int id , UpdateApprovalWorkflowDto updateApprovalWorkflowDto);
+        public bool DeleteApprovalWorkflow(int id);
+        public AddApprovalWorkflowDto AddApprovalWorkflow(int id, AddApprovalWorkflowDto addApprovalWorkflowDto);
+
     }
+
 }
